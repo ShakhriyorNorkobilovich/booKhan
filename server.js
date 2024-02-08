69ef349f276022fdbd95fac2c1a3b0e926961e95
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2024-02-08 20:23:56 +0900

    feat: loyihani shakillantirib oldik

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const dotenv = require("dotenv");
-dotenv.config();
+const http = require("http");
+// const fs = require("fs");
+// const app = require("./app");
+const mongodb = require("mongodb");
 
 
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true); // Set strictQuery to true
-
-
-const connectionString = process.env.MONGO_URL;
-mongoose.connect(
-    connectionString, 
-    {
+let db;
+const connectionString = "mongodb+srv://shaxroyorbek2101:Z@rifa3112@cluster0.rs251bp.mongodb.net/booKhan";
+mongodb.connect(connectionString, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    }, 
-    (err, goose) => {
+}, (err, client) => {
     if(err) console.log("ERROR on connection MongoDB");
     else{
         console.log("MongoDB connection succeed");
-        //console.log(goose);
-        const server = require("./app");
-        let PORT = process.env.PORT ||3007;
+        module.exports = client;
+        //console.log(client);
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3007;
         server.listen(PORT, function () {
-        console.log(
-            `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
-            );
-        });
+        console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
+});
     }
-}
-);
-
-
+});
